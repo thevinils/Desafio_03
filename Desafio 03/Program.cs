e1d6e256d3a691929ab5f6221c2a190e27960d18
luansoliver <rookie.qodeless@outlook.com>
2021-08-17 21:43:08 -0300

    Desafio 03

---
+++ Desafio 03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace LuanDesafio03
+{
+    class Program   
+    {
+        static void Main(string[] args)
+        {            
+
+            Console.WriteLine("Digite a Quantidade de Alunos:");
+            int Quantidade = int.Parse(Console.ReadLine());                      
+            Console.WriteLine("Qual o Nome do Aluno?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual a Nota do aluno?");
+            int nota = int.Parse(Console.ReadLine());
+            Console.WriteLine("Aluno com maior avaliação:");
+            Console.WriteLine(nome);
+            
+
+           
+        }
+
+
+        }
+
+            
+    }
+
