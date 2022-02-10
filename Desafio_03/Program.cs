754ae3087316a99fe4e67c63ca223fd0f86eca9a
Vinicius Lima <viniciuslssilva@hotmail.com>
2022-02-10 04:37:20 -0300

    ajuda com maior valor

--- Desafio_03/Program.cs
+++ Desafio_03/Program.cs
@@ -12,128 +12,99 @@ namespace Desafio_03
             
 
 
-            List <int> quantidalunos = new List<int>();
+            int quantidalunos = 0;
+            
             List <string> nomealunos = new List<string>();
             List <int> notalunos = new List<int>();
 
 
 
-            
 
-            /// QUANTIDADE ALUNOS ATÉ 3
-            
-
-            Console.WriteLine("(Aperte enter para começar, o sistema armazenará as notas e os nomes de 3 alunos)");
-
-            Console.ReadLine();
-
-            Console.WriteLine("Quantos alunos são?");
-
-            List<string> quantidalunos2 = quantidalunos.ConvertAll<string>(delegate (int i) { return i.ToString(); });
-
-            quantidalunos2.Add(Console.ReadLine());
-
-            Console.WriteLine("você incluirá..." +quantidalunos2[0]+"...alunos no sistema, aperte enter para continuar...");
-            
-            Console.ReadLine();
-
-            
-            /// ALUNO 1
-
-
-            Console.WriteLine("Escreva o nome do aluno, em seguida aperte enter e insira a nota do aluno");
-
-            nomealunos.Add(Console.ReadLine());
-
-
-            Console.WriteLine("insira a nota do aluno");
-
-            List<string> notalunos2 = notalunos.ConvertAll<string>(delegate (int i) { return i.ToString(); });
-
-            notalunos2.Add(Console.ReadLine());
-
-
-           
-            
-            Console.WriteLine("O aluno:..." + nomealunos[0] + "...Obteve a nota..." + notalunos2[0]);
-           
-            
 
 
             
+
+            /// QUANTIDADE ALUNOS
             
-            Console.WriteLine("Aperte enter para incluir mais um aluno");
 
-            Console.ReadLine();
+            
 
-
-            /// ALUNO 2
-
-
-
-
-            Console.WriteLine("Escreva o nome do aluno, em seguida aperte enter e insira a nota do aluno");
-
-            nomealunos.Add(Console.ReadLine());
-
-
-            Console.WriteLine("insira a nota do aluno");
-
-            notalunos2.Add(Console.ReadLine());
-
-            Console.WriteLine("O aluno:..." + nomealunos[1] + "...Obteve a nota..." + notalunos2[1]);
-
-
-            Console.WriteLine("Aperte enter para incluir mais um aluno");
-
-            Console.ReadLine();
-
-
-            /// ALUNO 3 
-
-
-
-
-            Console.WriteLine("Escreva o nome do aluno, em seguida aperte enter e insira a nota do aluno");
-
-            nomealunos.Add(Console.ReadLine());
-
-
-            Console.WriteLine("insira a nota do aluno");
-
-            notalunos2.Add(Console.ReadLine());
-
-            Console.WriteLine("O aluno:..." + nomealunos[2] + "...Obteve a nota..." + notalunos2[2]);
+            Console.WriteLine("Bem vindo, o sistema armazenará 3 alunos por vez, aperte enter para continuar");
 
             Console.WriteLine();
 
-            Console.WriteLine("Aperte enter para mostrar os alunos incluídos");
-
-
             Console.ReadLine();
 
-            ///// FOREACH
+            Console.WriteLine("Quantos alunos serão incluídos no sistema? '1 até 3' ");
 
 
-            foreach (string nome in nomealunos)
+
+
+
+            string quantidalunosst;
+            
+            quantidalunosst = quantidalunos.ToString();
+
+
+            quantidalunosst = Console.ReadLine();
+
+            Console.WriteLine("você incluirá..." + quantidalunosst +"...alunos no sistema, aperte enter para inserir nome e nota do aluno...");
+            
+            Console.ReadLine();
+
+            Console.WriteLine();
+
+
+            /// ALUNOS E NOTAS 
+
+
+            
+
+
+            for (quantidalunos = 0; quantidalunos < 3; quantidalunos++)
+
+
             {
-                Console.WriteLine(nome);
+
+                Console.WriteLine("Digite um nome para inserir na lista:");
+                string nome = Console.ReadLine();
+                nomealunos.Add(nome);
+
+                Console.WriteLine("Digite uma nota para inserir na lista:");
+
+                string notast;
+                int nota = 0;
+                notast = nota.ToString();
+
+                
+                notast = Console.ReadLine();
+                nomealunos.Add(notast);
+
+                Console.WriteLine("APERTE ENTER PARA INSERIR O PRÓXIMO ALUNO");
+
+                Console.WriteLine();
+
+                Console.ReadLine();
+
+
 
             }
 
+            Console.WriteLine("ALUNOS INSERIDOS, APERTE ENTER PARA VIZUALIZAR O ALUNO COM A MAIOR NOTA");
+
             Console.WriteLine();
 
-            Console.WriteLine("Aperte enter para mostrar o aluno com a maior nota");
-
             Console.ReadLine();
+
 
             //////// MAIOR NOTA
 
 
+            List<string> notalunosst = notalunos.ConvertAll<string>(delegate (int i) { return i.ToString(); });
 
-            var listaConcatenada = notalunos2.Concat(nomealunos);
+            List<string> listaconcatenada = nomealunos.Concat(notalunosst).ToList();
 
-            string maiornota = listaConcatenada.Max();
+            string maiornota = listaconcatenada.Max();
 
             Console.WriteLine("O aluno..." + maiornota + "...Obteve a maior nota da lista atual.");
 
